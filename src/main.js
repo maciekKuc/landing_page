5717eeb8bf533a0bbfe0539c2a82c11f5879a5da
Maciec <maciekkuc1@gmail.com>
2019-03-31 11:39:52 +0200

    container and basic
    added the container markup, and some functionality in js

---
+++ src/main.js
@@ -0,0 +1,12 @@
+const mySwiper = new Swiper ('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.news__button-next',
+      prevEl: '.news__button-prev',
+    }
+
+  })
