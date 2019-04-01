18309d7e1c3ffd7eed0ffb56c6124c2497b6dd4b
Maciec <maciekkuc1@gmail.com>
2019-04-01 22:59:49 +0200

    swiper rearanged
    changed the swiper markup, made swiper resposive and positioned the buttons

--- src/main.js
+++ src/main.js
@@ -1,7 +1,21 @@
 const newsSwiper = new Swiper ('.swiper-container', {
     
-    slidesPerView: 1,
-    slidesPerColumn: 2,
+    slidesPerView: 3,
+
+    breakpoints: {
+    // when window width is <= 320px
+    	500: {
+      	slidesPerView: 1,
+      	spaceBetween: 10
+    	},
+    // when window width is <= 480px
+    	768: {
+      	slidesPerView: 2,
+      	spaceBetween: 20
+    	}
+    // when window width is <= 640px
+    	
+  	},
     
     
     spaceBetween: 30,
@@ -16,18 +30,3 @@ const newsSwiper = new Swiper ('.swiper-container', {
 
   });
 
-const trendingSwiper = new Swiper ('.trending__swiper', {
-    
-    slidesPerView: 1,  
-    
-    spaceBetween: 30,
-    direction: 'horizontal',
-    loop: true,
-
-    
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    }
-
-  });
